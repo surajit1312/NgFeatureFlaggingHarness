e3ec4eeac7e0af473001ab6e4ab2afd4875e5ae6
Surajit Rana <surajit.rana@epicor.com>
2024-02-16 18:47:02 +0530

    fix[environment]: removed

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  sdkKey: '16bd0732-5f56-4cb7-8a45-554cee2cc8e7',
-  apiKey:
-    'sat.JIDDlhRcTD6T_YXDYOeW4A.65cc881019495407cb183efa.BQRDlbGRF6J39W1QEX09',
-  type: 'test',
-  account: 'JIDDlhRcTD6T_YXDYOeW4A',
-};
